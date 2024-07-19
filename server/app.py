9b5345c7aaef9a4cad8a6301dc64e031aa0da4a3
Sejal99 <sejal8974@gmail.com>
2024-07-19 16:44:51 +0530

    new commit

--- server/app.py
+++ server/app.py
@@ -1,20 +1,17 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,jsonify
+
+from config import Config
+from models import User,db
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATBASE_URI']='postgres://postgres:1234@localhost:5432/flask_database' 
-db=SQLAlchemy(app)
-class Task(db.model):
-    __table__ ='tasks'  #table name
-    id=db.Coloumn(db.Integer,primary_key=True,autoincrement=True)
-    title=db.Coloumn(db.String(200),nullable=False)
-    done=db.Coloumn(db.Boolean,dfault=False)
+app.config.from_object(Config)
+db.init_app(app)
 
-with app.app_context():
-    db.create_all()
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/users')
+def users():
+    return jsonify({'name':'Test user'})
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ server/config.py
@@ -0,0 +1,2 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/flask_database'
