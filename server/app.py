94d374387c531bfb50aa2b9588ef5a805e18ade7
sejal99 <sejal8974@gmail.com>
2024-07-20 19:37:16 +0530

    new commmit

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 from config import Config
 from models import User,db
@@ -7,10 +7,25 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
+#get users
+#if no method is passed then it is get by default
 @app.route('/users')
 def users():
     users=User.query.all()
-    return jsonify([user.to_dict() for user in users])   #convert users in dictionary format and return
+    return jsonify([user.to_dict() for user in users])
+
+@app.route('/users',methods=['POST'])
+def create_users():
+    data=request.get_json()
+    new_user=User(username=data['username'],email=data['email'])
+    db.session.add(new_user) ##add user to database
+    db.session.commit() #save the user
+  
+    return jsonify(new_user.to_dict())   #convert users in dictionary format and return
+
+
+#fetch single data
+@app.route('/users/<int:user_id>')
 
 if __name__ == '__main__':
     with app.app_context():
