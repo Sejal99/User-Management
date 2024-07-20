b5ed0ce1efa304f50469fa56932e38b4db81995d
sejal99 <sejal8974@gmail.com>
2024-07-20 19:24:07 +0530

    new commmit

--- server/app.py
+++ server/app.py
@@ -9,9 +9,10 @@ db.init_app(app)
 
 @app.route('/users')
 def users():
-    return jsonify({'name':'Test user'})
+    users=User.query.all()
+    return jsonify([user.to_dict() for user in users])   #convert users in dictionary format and return
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  #these two lines create tables in the database
     app.run(debug=True)
